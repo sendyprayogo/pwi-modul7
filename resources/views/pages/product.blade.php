0e3415cddbf53a565ea761493f74d229cd8d4c45
sendyprayogo <sendyprayogo@MacBook-Pro.local>
2025-11-02 21:27:36 +0700

    revisi deksripsi

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -23,15 +23,17 @@
                     <th scope="col">Nama Produk</th>
                     <th scope="col">Stok</th>
                     <th scope="col">Harga</th>
+                    <th scope="col">Deskripsi</th>
                     <th scope="col">Aksi</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <th scope="row">1</th>
-                    <td>Mark</td>
+                    <td>Biji Kopi Arabika Brazil</td>
                     <td>10</td>
-                    <td>Rp. 12.000</td>
+                    <td>Rp. 68.000</td>
+                    <td>Kopi brazil yang dikemas dalam 100gram.</td>
                     <td>
                         <button style="button" class="btn btn-danger">Delete</button>
                         <button style="button" class="btn btn-warning">Edit</button>
@@ -39,9 +41,10 @@
                 </tr>
                 <tr>
                     <th scope="row">2</th>
-                    <td>Jacob</td>
+                    <td>Pucuk Teh Inggris</td>
                     <td>19</td>
-                    <td>Rp. 19.000</td>
+                    <td>Rp. 96.000</td>
+                    <td>Teh Inggris yang dikemas dalam 100gram.</td>
                     <td>
                         <button style="button" class="btn btn-danger">Delete</button>
                         <button style="button" class="btn btn-warning">Edit</button>
@@ -49,9 +52,10 @@
                 </tr>
                 <tr>
                     <th scope="row">3</th>
-                    <td>Larry the Bird</td>
+                    <td>Sendok teh</td>
                     <td>4</td>
                     <td>Rp. 5.000</td>
+                    <td>Sendok teh untuk 10gram</td>
                     <td>
                         <button style="button" class="btn btn-danger">Delete</button>
                         <button style="button" class="btn btn-warning">Edit</button>
@@ -59,9 +63,10 @@
                 </tr>
                 <tr>
                     <th scope="row">4</th>
-                    <td>Nitro</td>
+                    <td>Filter Kopi</td>
                     <td>8</td>
-                    <td>Rp. 9.000.000</td>
+                    <td>Rp. 23.000</td>
+                    <td>100 pcs filter kopi merk kova</td>
                     <td>
                         <button style="button" class="btn btn-danger">Delete</button>
                         <button style="button" class="btn btn-warning">Edit</button>
